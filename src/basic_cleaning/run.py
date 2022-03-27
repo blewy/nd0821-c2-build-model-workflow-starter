c62fd6d8b4b16eaef176efe38fed4335983b8227
João Santos <jfasantos@gmail.com>
2022-03-27 09:38:19 +0100

    Patching basic_cleaning.py to remove latitude longitude outliers

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -40,6 +40,10 @@ def go(args):
     logger.info("Convert to datetime")
     df['last_review'] = pd.to_datetime(df['last_review'])
 
+    logger.info("Dropping Outliers by latitude and longitude")
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     filename = args.output_artifact
     df.to_csv(filename, index=False)
 
